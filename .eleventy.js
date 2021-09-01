3c54d14d554dde11ff5ef3536e8eb4530181142a
dcgoodwin2112 <dcgoodwin2112@gmail.com>
2021-09-01 16:08:55 -0400

    ruby shortcode updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addShortcode("ruby", function(char, rt) {
+    return `<ruby>${char}<rp>(</rp><rt>${rt}</rt><rp>)</rp></ruby>`
+  });
+
   return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
     templateFormats: ["md", "njk", "html"],
   }
 }
