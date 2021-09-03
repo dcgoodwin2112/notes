4a7c474910425d9c0298488a4da9efcb9b5dc10d
dcgoodwin2112 <dcgoodwin2112@gmail.com>
2021-09-03 12:10:28 -0400

    Added notes. Layout changes

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,27 @@
+//@ts-check
+const CleanCSS = require("clean-css")
+
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addShortcode("ruby", function(char, rt) {
+  eleventyConfig.setDataDeepMerge(true)
+
+  eleventyConfig.addFilter("cssmin", function (code) {
+    return new CleanCSS({}).minify(code).styles
+  })
+
+  eleventyConfig.addFilter("getCollectionHome", function(collection, page) {
+    const current = collection.find((cur) => {
+      return cur.url == page.url
+    })
+    return current.data.collectionHome
+  })
+
+  eleventyConfig.addShortcode("ruby", function (char, rt) {
     return `<ruby>${char}<rp>(</rp><rt>${rt}</rt><rp>)</rp></ruby>`
-  });
+  })
+
+  eleventyConfig.addCollection("jlptN5", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("notes/jlpt-n5-study-log/**/*.md")
+  })
 
   return {
     markdownTemplateEngine: "njk",
